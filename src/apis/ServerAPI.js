3966fd6a2d102fa48a2730f09f50d2fd5cad653c
ammarRBK <alshakmaskeain@gmail.com>
2018-08-05 19:16:35 +0300

    Mark as done tasks Feature worked fine Yahoooooooooooo

--- src/apis/ServerAPI.js
+++ src/apis/ServerAPI.js
@@ -3,17 +3,6 @@ const ServerAPI= {
     response: "",
     url: 'http://localhost:3000/api/',
     checkIsLoggedIn: function(){
-        // fetch(ServerAPI.url + 'users/checklogin')
-        //     .then(function(response){
-        //         return response.json();
-        //     })
-        //     .then(function(res) {
-        //         console.log(res);
-        //         if(res === "loggedin"){
-        //             ServerAPI.response= "True"
-        //         }
-        //         ServerAPI.response= "False";
-        //     });
         axios.get(ServerAPI.url + 'users/checklogin')
         .then(res => function(){
             if(res.data === "loggedin"){ return "True" } return "Hello Ammar";
